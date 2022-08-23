adf69b0a6280c14c7cc21882ca3a7ab9e9e1bf9c
JetPi <ibrahimzarragaam@gmail.com>
2022-08-23 22:29:10 +0000

    Done

--- src/models.py
+++ src/models.py
@@ -14,11 +14,29 @@ class User(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(150), nullable=False)
-    password = Column(String(25), nullable=False)
-    favorite_char_id = Column(Integer, ForeignKey("character.id"))
-    favorite_char = relationship("Character")
-    favorite_plan_id = Column(Integer, ForeignKey("planet.id"))
-    favorite_plan = relationship("Planet")
+    password = Column(String(100), nullable=False)
+
+    def __init__(self, user_name, user_password):
+        self.name = user_name
+        self.password = user_password
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    character_id = Column(Integer, ForeignKey("character.id"))
+    character = relationship("Character")
+    planet_id = Column(Integer, ForeignKey("planet.id"))
+    planet = relationship("Planet")
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship("User")
+
+    def add_favorite_char(self, char_id):
+        self.character_id = char_id 
+    
+    def add_favorite_plan(self, planet_id):
+        self.planet_id = planet_id 
 
 class Character(Base):
     __tablename__ = 'character'
@@ -26,7 +44,7 @@ class Character(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(150), nullable=False)
-    image = Column(String(250))     #?
+    image = Column(String(250), nullable=False)     
     age = Column(String(5), nullable=False)
     height = Column(String(5), nullable=False)
     # person_id = Column(Integer, ForeignKey('user.id'))
@@ -36,7 +54,7 @@ class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
     name = Column(String(150), nullable=False)
-    image = Column(String(250))     #?
+    image = Column(String(250), nullable=False)     #?
     rotation_period = Column(String(7), nullable=False)
     orbital_period = Column(String(7), nullable=False)
     gravity = Column(String(5), nullable=False)
