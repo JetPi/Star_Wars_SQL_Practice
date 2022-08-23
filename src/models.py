3828e5ea2831b3a4b0ca97dc039d203492f7f36a
JetPi <ibrahimzarragaam@gmail.com>
2022-08-23 01:46:29 +0000

    Test

--- src/models.py
+++ src/models.py
@@ -8,23 +8,39 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(150), nullable=False)
+    password = Column(String(25), nullable=False)
+    favorite_char_id = Column(Interger, ForeignKey("character.id"))
+    favorite_char = relationship("Character")
+    favorite_plan_id = Column(Integer, ForeignKey("planet.id"))
+    favorite_plan = relationship("Planet")
 
-class Address(Base):
-    __tablename__ = 'address'
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
+    name = Column(String(150), nullable=False)
+    image = Column(String(250))     #?
+    age = Column(String(5), nullable=False)
+    height = Column(String(5), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(Strin(150), nullable=False)
+    image = Column(String(250))     #?
+    rotation_period = Column(String(7), nullable=False)
+    orbital_period = Column(String(7), nullable=False)
+    gravity = Column(String(5), nullable=False)
+    terrain = Column(String(150), nullable=False)
 
     def to_dict(self):
         return {}
